f4f6d054ba339edb99664aed28bb3ffcabdf288b
Andrey <38962528+Chekavoo@users.noreply.github.com>
2024-04-30 12:47:37 +0400

    first commit

---
+++ src/config.js
@@ -0,0 +1,45 @@
+const config = {
+    cards: [
+        {
+            id: 1,
+            name: "chess",
+            image: "chess.svg"
+        },
+        {
+            id: 2,
+            name: "console",
+            image: "console.svg"
+        },
+        {
+            id: 3,
+            name: "controller",
+            image: "controller.svg"
+        },
+        {
+            id: 4,
+            name: "crown",
+            image: "crown.svg"
+        },
+        {
+            id: 5,
+            name: "memory",
+            image: "memory.svg"
+        },
+        {
+            id: 6,
+            name: "mushroom",
+            image: "mushroom.svg"
+        },
+        {
+            id: 7,
+            name: "pacman",
+            image: "pacman.svg"
+        },
+        {
+            id: 8,
+            name: "puzzle",
+            image: "puzzle.svg"
+        },
+    ]
+}
+export default config;
